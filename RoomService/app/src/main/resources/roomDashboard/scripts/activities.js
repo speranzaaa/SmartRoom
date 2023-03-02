604bb92263b276d0221b98e9f96d85f02c337b13
Alessandro <alessandro.antonini6@studio.unibo.it>
2023-03-02 11:26:08 +0100

    update light switch position on new sse activities

--- RoomService/app/src/main/resources/roomDashboard/scripts/activities.js
+++ RoomService/app/src/main/resources/roomDashboard/scripts/activities.js
@@ -1,5 +1,12 @@
 var source = new EventSource('activities');
 
+//change switch position if model change (based on new activities)
+source.addEventListener("lights-subgroup", function(event) {
+    var jsonActivity = JSON.parse(event.data);
+    lightSwitch.setOn(jsonActivity.status.on);
+})
+
+//append new activities
 source.addEventListener("lights-subgroup", function(event) {
     var activity = document.createElement("li")
     activity.innerHTML = event.data;
