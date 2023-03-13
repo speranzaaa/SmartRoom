0262d31ec0a36620b8ddff1ff7ed77fe5e004045
Simone Zama <simone.zama@studio.unibo.it>
2023-03-13 14:11:15 +0100

    Add serial communication to RoomImpl

--- RoomService/app/src/main/java/RoomService/room/RoomImpl.java
+++ RoomService/app/src/main/java/RoomService/room/RoomImpl.java
@@ -8,19 +8,31 @@ import RoomService.activities.ActivityImpl;
 import RoomService.activities.ActivityLogger;
 import RoomService.devices.Device;
 import RoomService.devices.actuators.Actuator;
+import RoomService.devices.actuators.LightImpl.LightStatus;
+import RoomService.devices.actuators.RollerBlindsImpl.RollerBlindStatus;
+import RoomService.serial.SerialCommChannel;
+import RoomService.serial.SerialStatusMessenger;
 
 public class RoomImpl implements Room {
 	
 	private final Map<String, Device> devices = new HashMap<>();
 	private final ActivityLogger activityLogger;
+	private final SerialStatusMessenger messenger;
 	
-	public RoomImpl(ActivityLogger activityLogger) {
-		this.activityLogger = activityLogger;
+	public RoomImpl(ActivityLogger activityLogger) throws Exception {
+		this.activityLogger = activityLogger;		
+		this.messenger = new SerialStatusMessenger(new SerialCommChannel());
 	}
 
 	@Override
 	public void addActuator(Actuator actuator) {
 		actuator.addStatusObserver((status)->this.activityLogger.logActivity(new ActivityImpl(actuator, status)));
+		// Add listener to send status update via serial port
+		if (actuator.getName().equals("lights-subgroup")) {
+			actuator.addStatusObserver((status) -> this.messenger.sendUpdate((LightStatus)status));
+		} else {
+			actuator.addStatusObserver((status) -> this.messenger.sendUpdate((RollerBlindStatus)status));
+		}
 		this.devices.put(actuator.getName(), actuator);
 	}
 
